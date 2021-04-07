7d7dd30c28e0eb352d70c6b35716aff731d0d09c
technique-VPSI <technique.vpsi@gmail.com>
2021-04-07 10:50:32 +0000

    add dealer infos

--- ass/models/.ipynb_checkpoints/sav_ticket-checkpoint.py
+++ ass/models/.ipynb_checkpoints/sav_ticket-checkpoint.py
@@ -6,26 +6,22 @@ from odoo import models, fields , api
 class ctr(models.Model):
     _inherit = 'helpdesk.ticket'
     sequence = fields.Char(string="N° ticket", readonly=True, required=True, copy=False, default='New')
+    reference=fields.Char(string="Réference")
+    #objet_reclamation=fields.Char(string="Objet de la reclamation")
+    #date_reclamation=fields.Date(string="Date de raclamation")
     cust=fields.Many2one(comodel_name="res.partner",string="Partenaire")
     phone = fields.Char(related="cust.mobile",string="Téléphone")
+    applicant_name = fields.Many2one(comodel_name="res.partner",string="Demandeur")
+    applicant_phone = fields.Char(related="applicant_name.mobile",string="Téléphone")
+    dealer_name =  fields.Many2one(comodel_name="res.partner",string="Revendeur")
+    dealer_phone = fields.Char(related="dealer_name.mobile",string="Téléphone")
     product = fields.Many2one(comodel_name="product.product",string="Produit")
     mark = fields.Many2one(comodel_name="product.category",string="Marque")
-    reference=fields.Char(string="Réference")
-    objet_reclamation=fields.Char(string="Objet de la reclamation")
-    date_reclamation=fields.Date(string="Date de raclamation")
-    date_proch_action=fields.Date(string="Date de prochain action")
+    product_model = fields.Char(string="Modèle Produit")
+    product_serial = fields.Char(string="Numéro de serie")
+   
 
     @api.model
     def create(self, vals):
         vals['sequence'] = self.env['ir.sequence'].next_by_code('sav.ticket')
         return super(ctr, self).create(vals)
-
-""" class product(models.Model):
-    _inherit= 'helpdesk.ticket'
-    code_produit=fields.Char(string='Code')
-    libelle_produit=fields.Char(string='Produit')
-
-class marque(models.Model):
-    _inherit= 'helpdesk.ticket'
-    code_marque=fields.Char(string='Code')
-    libelle_marque=fields.Char(string='Marque') """
